e48bee6e6d70244c2bc0a9ef35aa028e1404eb6c
Liebenfels-18 <nik.yarych2001@yandex.ru>
2023-10-10 00:03:44 +0300

    second task

--- src/test/java/ru/netology/services/CalculateServiceTest.java
+++ src/test/java/ru/netology/services/CalculateServiceTest.java
@@ -3,38 +3,21 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CalculateServiceTest {
 
-    @Test
-    void shouldCalculateFirstParameters() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resource/parameters.csv")
+    public void calculateCountMonths(int expected, int income, int expenses, int threshold) {
         CalculateService service = new CalculateService();
-
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-
-        int expected = 3;
 
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateSecondParameters() {
-        CalculateService service = new CalculateService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
-
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
 
 
 }
